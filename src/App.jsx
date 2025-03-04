1348ad97ecd687922ab1a7a1c5d4cb71edd4cf73
zazaborisovi <geozedx123@mail.ru>
2025-03-04 21:01:34 +0400

    Changed background added more courses and changed design of them

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Facebook } from 'lucide-react';
+import CourseDivs from './components/CourseDivs.jsx';
 
 export default function App() {
   const scrollToSection = (id) => {
-      document.getElementById(id).scrollIntoView({ top: document.getElementById(id).offsetTop-80 , behavior: "smooth" });
-    };
+      document.getElementById(id).scrollIntoView({ top: document.getElementById(id).offsetTop , behavior: "smooth" })
+    }
   const headerList = [
       {"li": "Main"},
       {"li": "Courses"}
@@ -16,20 +17,10 @@ export default function App() {
       {"h1": "GOA არის მსოფლიოში საუკეთესო აკადემია!" , "link": "https://www.facebook.com/share/p/15HB56TKk8/"},
       {"h1": "ყველაზე მეგობრული გარემო სადაც შემოსვლას არ ინანებთ ეს გოაა ❤ ❤" , "link": "https://www.facebook.com/share/p/18nfXJCRCe/"},
   ]
-  const programmingCourse = [
-    {"li": "Programming Basics With Python"},
-    {"li": "Html & Css"},
-    {"li": "Git & Github"},
-    {"li": "Javascript"},
-    {"li": "React"},
-    {"li": "Django"},
-    {"li": "MYSQL"},
-    {"li": "AI(Artificial Inteligence) & Algorithms"}
-  ]
   return (
-    <div className='bg-slate-800'>
+    <div className='bg-black '>
       <header className='bg-black h-20 w-full *:text-xl *:text-emerald-800 flex items-center *:h-max overflow-hidden justify-between sticky top-0'>
-        <img src="c:/Users/x/Desktop/goa/GOA CERTIFICATE PROJECT/src/assets/goa logo.jpg" className='w-30 ml-10'/>
+        <img src="../assets/goaLogo.jpg" alt='' className='w-30 ml-10'/>
         <ul className='flex gap-5'>
           {
             headerList.map((item , index) => (
@@ -40,7 +31,7 @@ export default function App() {
         <button className='mr-10' onClick={() => window.open('https://www.facebook.com/nika11keshelava/', '_blank')}>Join GOA</button>
       </header>
       <div className='h-auto pt-20 pb-20 flex justify-around' id='section1'>
-        <div className='w-[35%] h-[400px] flex border-2 border-black p-10 rounded-4xl bg-black'>
+        <div className='w-[35%] h-[400px] flex border-2 border-black p-10 rounded-4xl bg-black shadow-[0px_1px_20px_5px] shadow-green-800'>
           <p className='text-2xl w-fit font-sans font-extrabold flex items-center text-emerald-800'>
           GOA (Goal-Oriented Academy) isn't just the best place to learn programming—it’s where you become the best version of yourself. 
           At GOA, you don’t just master coding; you develop leadership skills, a strategic mindset, and the discipline to thrive in any challenge. 
@@ -48,7 +39,7 @@ export default function App() {
           and confidence to succeed as both a programmer and a person
           </p>
         </div>
-        <div className='w-[35%]'>
+        <div className='w-[35%] '>
         <iframe className='w-full h-full'
         src="https://www.youtube.com/embed/APg-_0_K6dc?si=74ZfRDpdfWA3xTnU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
         </iframe>
@@ -56,7 +47,7 @@ export default function App() {
       </div>
       <div className='h-[250px] flex items-center flex-col gap-5 m-[50px]'>
         <h1 className='text-xl'>Reviews:</h1>
-        <div className='flex gap-5 overflow-hidden justify-center w-full ScrollingDivs'>
+        <div className='flex gap-5 justify-center w-full ScrollingDivs *:shadow-[0px_1px_20px_5px] *:shadow-green-800'>
           {
               reviews.map((item , index) => (
                   <div className='border-2 h-[200px] w-[20%] rounded-2xl bg-black *:text-green-800 flex flex-col items-center gap-2 *:text-xl p-5'>
@@ -70,17 +61,8 @@ export default function App() {
       </div>
       <div className='h-auto pt-20 flex flex-col gap-10 items-center pb-20' id='section2'>
         <h1 className='text-2xl'>Courses</h1>
-        <div className='w-[100%] flex pl-10 pr-10'>
-          <div className='h-[450px] border-2 border-black w-[25%] *:text-xl flex items-center flex-col p-10 gap-7 rounded-2xl bg-black text-green-800'>
-            <h1 className='text-2xl border-b-2 w-[100%] text-center pb-4'>Programming Course</h1>
-            <ol className='list-decimal flex flex-col gap-3' start={0}>
-            {
-              programmingCourse.map((item , index) => (
-                <li>{item.li}</li>
-              ))
-            }
-            </ol>
-          </div>
+        <div className='w-[100%] flex pl-10 pr-10 h-[500px] justify-around'>
+          <CourseDivs />
         </div>
       </div>
     </div>
---
+++ src/components/CourseDivs.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from 'react'
+
+export default function CourseDivs() {
+    const [active , setActive] = useState(null)
+    const Courses = [
+        {
+            title: "Programming Course",
+            listedItems: [
+                "Programming Basics With Python",
+                "Html & Css",
+                "Git & Github",
+                "Javascript",
+                "React",
+                "Django",
+                "MYSQL",
+            ],
+        },
+        {
+            title: "Algorithms & AI",
+            listedItems: [
+                "Algorithms",
+                "C++",
+                "AI"
+            ]
+        },
+        {
+            title: "Game Development",
+            listedItems: [
+                "Roblox Studio",
+                "Lua",
+                "Scripts, Animations, and GUI",
+                "Servers And Clients"
+            ]
+        },
+        {
+            title: "Graphic Design",
+            listedItems: [
+                "Adobe Photoshop and Adobe Illustrator",
+                "Photoshop Interface",
+                "Working With Photoshop Applications"
+            ]
+        }
+    ]
+    const handleActive = (index) => {
+        setActive((prevActive) => {
+            if (prevActive === index) {
+                return null
+            } else {
+                return index
+            }
+        })
+    }
+    const details = (item , index) => {
+        if (active === index){
+            return(
+                <div id={`detail${index}`} className='flex items-end flex-col'>
+                    <ol className='text-lg list-decimal flex flex-col w-[80%] items-start' start={0}>
+                    {
+                    item.listedItems.map((i) => (
+                        <li>{i}</li>
+                    ))}
+                    </ol>
+                </div>
+            )
+        }
+    }
+    return (
+        <>
+        {
+            Courses.map((item , index) => (
+                <div 
+                className='
+                h-fit w-[20%] bg-black *:text-green-800 border-2 border-black rounded-2xl flex flex-col items-center gap-5 p-4 
+                shadow-[0px_1px_20px_5px] shadow-green-800
+                '>
+                    <h1 className='text-2xl'>{item.title}</h1>
+
+                    {details(item , index)}
+
+                    <div className='border-t-2 w-fit border-b-2 hover:border-white hover:text-white cursor-default' id={`details${index}`}
+                    onClick={() => handleActive(index) }>
+                        {active === index ? 'Click to hide details' : 'Click to show details'}
+                    </div>
+                </div>
+            ))
+        }
+        </>
+  )
+}
